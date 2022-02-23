2889dd540385fb0fb70c6c486115b4f9bc7793db
Jon Dufty <jonathan.dufty@99designs.com>
2022-02-23 19:11:29 +1100

    Add login route and user models

--- auth/models/user.go
+++ auth/models/user.go
@@ -1,4 +1,4 @@
-package auth
+package models
 
 import "time"
 
@@ -8,4 +8,24 @@ type User struct {
 	Email        string    `json:"email" meddler:"email"`
 	TimeCreated  time.Time `json:"time_created" meddler:"time_created"`
 	PasswordHash string    `json:"password_hash" meddler:"password_hash"`
+}
+
+func New(name string, email string, pwdHash string) *User {
+	return &User{
+		ID:           123,
+		FullName:     name,
+		Email:        email,
+		TimeCreated:  time.Now(),
+		PasswordHash: pwdHash,
+	}
+}
+
+func NewTest() *User {
+	return &User{
+		ID:           123,
+		FullName:     "Test user",
+		Email:        "test@example.com",
+		TimeCreated:  time.Now(),
+		PasswordHash: "jsdnabfljvbnasflv",
+	}
 }
--- auth/routes.go
+++ auth/routes.go
@@ -1,14 +1,25 @@
 package auth
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
+	"time"
 
 	authpb "github.com/jonDufty/recipes/auth/rpc/authpb"
+
+	"github.com/jonDufty/recipes/auth/models"
+	"github.com/jonDufty/recipes/common/crypto"
 
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
 )
+
+type AuthPostParams struct {
+	Email    string `json:"email"`
+	Password string `json:"password"`
+	FullName string `json:"full_name"`
+}
 
 // Returns chi router with common middleware applied
 // Middleware includes:
@@ -19,8 +30,17 @@ func NewRouter(a *App) http.Handler {
 	// Add common middleware
 	r.Use(middleware.Logger)
 
-	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
+	r.Get("/system/healthcheck", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(w, "🔐")
+	})
+
+	// Common auth endpoints
+	r.Group(func(r chi.Router) {
+		// Other middleware
+
+		r.Post("/login", login)
+		r.Post("/register", register)
+
 	})
 
 	// Add twirp routes
@@ -31,4 +51,45 @@ func NewRouter(a *App) http.Handler {
 	}))
 
 	return r
+}
+
+func getPostParams(r *http.Request) (AuthPostParams, error) {
+	d := AuthPostParams{}
+	decoder := json.NewDecoder(r.Body)
+	err := decoder.Decode(&d)
+
+	if err != nil {
+		return d, err
+	}
+
+	return d, nil
+}
+
+func register(w http.ResponseWriter, r *http.Request) {
+
+	d, err := getPostParams(r)
+	if err != nil {
+		http.Error(w, "Invalid request body", http.StatusBadRequest)
+	}
+
+	hash, err := crypto.HashPassword(d.Password)
+	if err != nil {
+		http.Error(w, "Invalid request body", http.StatusBadRequest)
+	}
+
+	requestUser := &models.User{
+		FullName:     d.FullName,
+		Email:        d.Email,
+		TimeCreated:  time.Now(),
+		PasswordHash: hash,
+	}
+
+	fmt.Print(d)
+	fmt.Printf("%v", *requestUser)
+	w.Write([]byte("Hello there"))
+
+}
+
+func login(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Hello there"))
 }
