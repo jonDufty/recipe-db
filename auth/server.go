7ddd2e4b00a1766526aebf77f17f1c392be42130
Jon Dufty <jonathan.dufty@99designs.com>
2022-03-28 12:28:34 +1100

    Move login and signup to twirp endpoints

--- auth/server.go
+++ auth/server.go
@@ -5,6 +5,7 @@ import (
 	"log"
 
 	"github.com/jonDufty/recipes/auth/mappers"
+	"github.com/jonDufty/recipes/auth/models/user"
 	pb "github.com/jonDufty/recipes/auth/rpc/authpb"
 	"github.com/jonDufty/recipes/common/crypto"
 	"github.com/twitchtv/twirp"
@@ -42,8 +43,32 @@ func (s *Server) GetUserById(c context.Context, r *pb.ByIdRequest) (*pb.User, er
 }
 
 func (s *Server) LoginWithPassword(c context.Context, r *pb.LoginRequest) (*pb.LoginResponse, error) {
+
+	if r.Email == "" || r.Password == "" {
+		return nil, twirp.NewError(twirp.InvalidArgument, "Some fields are empty")
+	}
+
+	u, err := user.GetUserByEmail(c, r.Email)
+	if err != nil {
+		log.Printf("User of email: %s not found", r.Email)
+		log.Print(err.Error())
+		return nil, twirp.NewError(twirp.InvalidArgument, "")
+	}
+
+	err = crypto.CheckPassword(r.Password, u.PasswordHash)
+	if err != nil {
+		log.Printf("Password for email: %s does not match hash\nExpected: %s\n Received %s", r.Email, u.PasswordHash, r.Password)
+		log.Print(err.Error())
+		return nil, twirp.NewError(twirp.InvalidArgument, "")
+	}
+
+	token, err := GenerateToken(u)
+	if err != nil {
+		return nil, twirp.NewError(twirp.Internal, "Failed to generate token")
+	}
+
 	return &pb.LoginResponse{
-		Token: "fake-token",
+		Token: token,
 	}, nil
 }
 
@@ -68,12 +93,23 @@ func (s *Server) SignupWithPassword(c context.Context, r *pb.SignupRequest) (*pb
 
 	log.Print("User created successfully")
 
+	token, err := GenerateToken(requestUser)
+	if err != nil {
+		return nil, twirp.NewError(twirp.Internal, "Failed to generate token")
+	}
+
 	return &pb.LoginResponse{
-		Token: "fake-token",
+		Token: token,
 	}, nil
 }
 
 func (s *Server) ValidateToken(c context.Context, r *pb.ValidateTokenRequest) (*pb.ValidateTokenResponse, error) {
+
+	_, err := ValidateToken(r.Token)
+	if err != nil {
+		return nil, twirp.NewError(twirp.InvalidArgument, "Invalid token")
+	}
+
 	return &pb.ValidateTokenResponse{
 		Valid: true,
 	}, nil
