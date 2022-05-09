f7479b298aaa0f2fdc56c8079dc6b5fb55b15b6d
Jon Dufty <jonathan.dufty@99designs.com>
2022-05-09 19:48:44 +1000

    Run gqlgen init

---
+++ graph/server.go
@@ -0,0 +1,27 @@
+package graph
+
+import (
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/99designs/gqlgen/graphql/handler"
+	"github.com/99designs/gqlgen/graphql/playground"
+	"github.com/jonDufty/recipes/graph/generated"
+	"github.com/urfave/cli/v2"
+)
+
+func ServeGraph(c *cli.Context) error {
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
+
+	srv := handler.NewDefaultServer(generated.NewExecutableSchema(generated.Config{Resolvers: &Resolver{}}))
+
+	http.Handle("/", playground.Handler("GraphQL playground", "/query"))
+	http.Handle("/query", srv)
+
+	log.Printf("connect to http://localhost:%s/ for GraphQL playground", port)
+	return http.ListenAndServe(":"+port, nil)
+}
